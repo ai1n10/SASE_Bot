2bad0d6eca7f3f79519319590b4ac3498cfabbb7
ai1n10 <adisak4u@gmail.com>
2017-02-17 17:51:09 +0700

    Update SASE_bot.php

--- SASE_bot.php
+++ SASE_bot.php
@@ -21,15 +21,8 @@ if (!is_null($events['events'])) {
 			$messages = [
 				{'type' => 'text',
 				'text' => $text
-				},
-				{
-				'type' => 'text',
-				'text' => "Hi, there"
-				},
-				{
-				'type' => 'text',
-				'text' => "May I help you?"				   
 				}
+
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
